e329dbd4ed5333290ab452e57214b23d5fb7e01b
Eddie Yao <adpoyao@gmail.com>
2017-12-21 08:35:47 -0800

    adds bubble sort

---
+++ README.md
@@ -0,0 +1,14 @@
+# QUESTION 1
+Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
+
+## STRATEGY
+
+# QUESTION 2
+Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array).
+
+## STRATEGY
+
+# QUESTION 3
+Imagine that I gave you twenty books to sort in alphabetical order. How would you go about it? Can you express this as an algorithm?
+
+## STRATEGY
--- drills.js
+++ drills.js
@@ -1,12 +1,24 @@
 'use strict';
 
 //Bubble Sort
-function swap(){
-
+function swap(array, i, j){
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 }
 
-function bubbleSort(){
-
+function bubbleSort(array){
+  let swaps = 0;
+  for(let i=0; i<array.length-1; i++){
+    if(array[i] > array[i+1]){
+      swap(array, i, i+1);
+      swaps++;
+    }
+  }
+  if(swaps>0){
+    return bubbleSort(array);
+  }
+  return array;
 }
 
 
@@ -26,5 +38,5 @@ function quickSort(){
 }
 
 function partition(){
-  
+
 }
