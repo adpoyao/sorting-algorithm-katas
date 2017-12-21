a488b43786876b0f12081cb5012ba5eeae5573e1
Eddie Yao <adpoyao@gmail.com>
2017-12-21 08:17:31 -0800

    adds function stubs

--- drills.js
+++ drills.js
@@ -1,16 +1,30 @@
 'use strict';
 
 //Bubble Sort
+function swap(){
+
+}
+
 function bubbleSort(){
 
 }
+
 
 //Merge Sort
 function mergeSort(){
 
 }
 
-//Quick Sort
+function merge(){
+
+}
+
+
+//Quick Sort + Swap
 function quickSort(){
+
+}
+
+function partition(){
   
 }
