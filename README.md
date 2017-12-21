53ad92db086577053569efa99afaecb9230390d7
Adam Bradbury <f.adambrad@gmail.com>
2017-12-21 16:36:47 -0700

    sorter algo uploaded

--- README.md
+++ README.md
@@ -2,13 +2,59 @@
 Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
 
 ## STRATEGY
+[ 3, 7, 5, 2] =>
+[ x, x,  , x,  , x]
+[ 2, 3, 5, 7]
+
+<!-- 
+[30, 62, 10, 74, 24, 40, 50] =>
+[10,   ,   ,   ,   ,   , 74] -->
+
+* Lowest: 10
+* Highest: 74
+
+1. Start with tempArray: Array(max-min+1) example: 6 slots;
+2. Loop through original array and plug: tempArray[mainArray[i]-min] = 'flag';
+3. Loop through tempArray and plug:
+    - Condition: if tempArray[i] value = flag: newArrayPush(i+min); 
+    -            if not, carry on;
+4. Return newArray;
 
 # QUESTION 2
 Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array).
 
 ## STRATEGY
+[3, 7, 5, 2]
+
+1. Create a LOOP;
+2. Create a random index based on length: 
+3. Let temp: array[random];
+4. array[random] = array[i];
+5. array[i] = array[random]
+6. Returns original Array;
 
 # QUESTION 3
 Imagine that I gave you twenty books to sort in alphabetical order. How would you go about it? Can you express this as an algorithm?
+
+["To Kill a Mockingbird", 
+"Pride and Prejudice", 
+"The Diary of Anne Frank", 
+"1984",
+"Harry Potter and the Sorcerer's Stone",
+"The Lord of the Rings",
+"The Great Gatsby",
+"Charlotte's Web",
+"The Hobbit",
+"Little Women",
+"Fahrenheit 451",
+"Jane Eyre",
+"Animal Farm",
+"Gone with the Wind",
+"The Catcher in the Rye",
+"The Book Thief",
+"The Adventures of Huckleberry Finn",
+"The Hunger Games",
+"The Help",
+"The Lion, the Witch, and the Wadrobe"]
 
 ## STRATEGY
--- drills.js
+++ drills.js
@@ -23,44 +23,70 @@ function bubbleSort(array){
 
 
 //Merge Sort
-function mergeSort(array){
-  if(array.length <= 1){
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return merge(left, right, array);
+};
+
+function merge(left, right, array) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let outputIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            array[outputIndex++] = left[leftIndex++];
+        }
+        else {
+            array[outputIndex++] = right[rightIndex++];
+        }
+    }
+
+    for (let i=leftIndex; i<left.length; i++) {
+        array[outputIndex++] = left[i];
+    }
+
+    for (let i=rightIndex; i<right.length; i++) {
+        array[outputIndex++] = right[i];
+    }
     return array;
-  }
-  let mid = Math.floor(array.length/2);
-  let left = array.slice(0, mid);
-  let right = array.slice(mid, array.length);
+};
 
-  left = mergeSort(left);
-  right = mergeSort(right);
-  return merge(left, right, array);
-}
+// const number = [2,4,1,3,8,5]
+// const books = ["To Kill a Mockingbird", 
+// "Pride and Prejudice", 
+// "The Diary of Anne Frank", 
+// "1984",]
+// "Harry Potter and the Sorcerer's Stone",
+// "The Lord of the Rings",
+// "The Great Gatsby",
+// "Charlotte's Web",
+// "The Hobbit",
+// "Little Women",
+// "Fahrenheit 451",
+// "Jane Eyre",
+// "Animal Farm",
+// "Gone with the Wind",
+// "The Catcher in the Rye",
+// "The Book Thief",
+// "The Adventures of Huckleberry Finn",
+// "The Hunger Games",
+// "The Help",
+// "The Lion, the Witch, and the Wadrobe"]
 
-function merge(left, right, array){
-  let leftIndex = 0;
-  let rightIndex = 0;
-  let outputIndex = 0;
-  // [1,4,3] [2,5,6]
-  while(leftIndex < left.length && rightIndex < right.length){
-    if(left[leftIndex] < right[rightIndex]){
-      array[outputIndex++] = left[leftIndex];
-    }
-    else {
-      array[outputIndex++] = right[rightIndex];
-    }
-  }
-  for(let i = leftIndex; i < left.length; i++){
-    array[outputIndex++] = left[i];
-  }
-  for(let i = rightIndex; i < right.length; i++){
-    array[outputIndex++] = right[i];
-  }
-  return array;
-}
+// console.log(mergeSort(number))
 
 
 //Quick Sort + Swap
-[1, 2, 3, 7, 4]; //length: 5
+// [1, 2, 3, 7, 4]; //length: 5
 function quickSort(array, start=0, end=array.length-1){
   //base case
   if(start >= end){
@@ -84,3 +110,88 @@ function partition(array, start, end){
   swap(array, end, j);
   return j;
 }
+
+//EXERCISE 1
+
+function sortWithMinMax(array, max, min){
+  const tempArray = Array(max - min+1);
+  const newArray = [];
+  for(let i=0; i < array.length; i++){
+    tempArray[array[i]-min] = 'x';
+  }
+  for(let i=0; i < tempArray.length; i++){
+    if(tempArray[i] === 'x'){
+      newArray.push(i+min);
+    }
+  }
+  return newArray;
+}
+
+const test1= [ 3, 7, 5, 2];
+const test2 = [-1, 2, 6, 4, 8];
+
+// console.log(sortWithMinMax(test1, 7, 2));
+// console.log(sortWithMinMax(test2, 8, -1));
+
+//EXERCISE 2
+
+let arr = [3, 7, 5, 2]
+
+function shuffleArray(array){
+    let temp;
+    for(let i = 0; i < array.length; i++){
+        let randomIdx = Math.floor((Math.random() * array.length - 1) + 1)
+        console.log(randomIdx, 'randomIdx')
+         temp = array[i];
+         array[i] = array[randomIdx]
+         array[randomIdx] = temp         
+    }
+    return array
+}
+
+// console.log(shuffleArray(arr));
+
+function swap(array, i, j) {
+    console.log(array, 'array', i, 'i', j, 'j')
+    const tmp = array[i];
+    console.log(tmp, 'tmp')
+    array[i] = array[j];
+    array[j] = tmp;
+};
+
+function bubbleSort(array) {
+    let swaps = 0;
+    for (let i=0; i<array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1);
+            swaps++;
+        }
+    }
+
+    if (swaps > 0) {
+        return bubbleSort(array);
+    }
+    return array;
+};
+
+
+// console.log(bubbleSort(books))
+
+const books = [ 'To Kill a Mockingbird','Pride and Prejudice','The Diary of Anne Frank',
+'1984' ] 
+ 
+const numbers = ['bob', 'adam', 'doug', 'cara']
+
+function sorter(array){
+    let tempArr = [];
+    for(let i = 0; i < array.length; i++){
+        console.log(i, 'i')
+        for(let j = 0; j<tempArr.length && tempArr[j] <= array[i]; j++){
+            console.log(j, 'j')
+            tempArr.splice([j], 0, array[i])
+        }
+    }
+    return tempArr
+}
+
+console.log(sorter(numbers))
